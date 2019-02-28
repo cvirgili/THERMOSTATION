477046ce0c27236c4e07b66b83d10f02a28e6562
cvirgili <cristiano.virgili@gmail.com>
2019-02-28 23:00:00 +0100

    ok scheduler

--- modules/Scheduler.js
+++ modules/Scheduler.js
@@ -22,19 +22,19 @@ module.exports = class Scheduler {
     }
 
     getJobsOfTheDay(day) {
-        let timerObject = this.resetTimerArray();
-        timerObject.today = day;
+        Scheduler.timerObject = this.resetTimerArray();
+        Scheduler.timerObject.today = day;
         Scheduler.schedData.week.find((item) => { return item.id == day; }).jobs.forEach((job) => {
             for (let h = job.on.hour; h <= job.off.hour; h++) {
                 let last = (h === job.off.hour) ? job.off.minute : 60;
                 let first = (h === job.on.hour) ? job.on.minute : 0;
                 for (let m = first; m < last; m++) {
-                    timerObject[h + "-" + m].val = 1;
-                    timerObject[h + "-" + m].treshold = job.on.treshold;
+                    Scheduler.timerObject[h + "-" + m].val = 1;
+                    Scheduler.timerObject[h + "-" + m].treshold = job.on.treshold;
                 }
             }
         });
-        return timerObject;
+        return Scheduler.timerObject;
     }
 
     start() {
@@ -43,26 +43,26 @@ module.exports = class Scheduler {
             Scheduler.schedData = JSON.parse(fs.readFileSync(__basedir + '/data/scheduler.json'));
             this.isStart = true;
             console.log("scheduler start");
-            this.loop(this.getJobsOfTheDay(new Date().getDay()));
+            this.loop();
             resolve(true);
         });
     }
 
-    loop(timerObject) {
+    loop() {
         if (this.isStart == false) {
             clearTimeout(Scheduler.schedulerTimeout);
             return;
         }
         let now = new Date();
-        if (timerObject.today != now.getDay()) {
+        if (Scheduler.timerObject.today != now.getDay()) {
             this.stop().then(() => {
                 clearTimeout(Scheduler.schedulerTimeout);
                 this.start();
                 return;
             });
         }
-        BoilerController.setRelay(timerObject[now.getHours() + "-" + now.getMinutes()].val).catch(console.error);
-        let reloop = () => { this.loop(timerObject); };
+        BoilerController.setRelay(Scheduler.timerObject[now.getHours() + "-" + now.getMinutes()].val).catch(console.error);
+        let reloop = () => { this.loop(); };
         Scheduler.schedulerTimeout = setTimeout(reloop, 5000);
     }
 
@@ -82,9 +82,9 @@ module.exports = class Scheduler {
                 if (!err) {
                     if (body != JSON.stringify(Scheduler.schedData)) {
                         console.log("NEW SCHED", JSON.parse(body));
-                        //Scheduler.schedData = JSON.parse(body);
+                        Scheduler.schedData = JSON.parse(body);
                         fs.writeFile(__basedir + '/data/scheduler.json', body, (err) => {
-                            this.stop().then(() => { this.start(); }).catch(console.error);
+                            this.getJobsOfTheDay(new Date().getDay());
                         });
                     }
                 } else {
@@ -97,4 +97,4 @@ module.exports = class Scheduler {
         let interval = setInterval(loop, 5000);
     }
 
-};
+}
