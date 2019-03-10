6fe295d2083392437ccd9bb8f796c98fb7035cee
cvirgili <cristiano.virgili@gmail.com>
2019-03-10 23:45:35 +0100

    ADDED PHP MYSQL DB SCRIPT

--- read_boiler_status.php
+++ read_boiler_status.php
@@ -11,7 +11,30 @@
 	// echo fread($myfile,filesize($filename));
 
 	// fclose($myfile);
+	$servername = "localhost";
+	$username = "chnlwysx_wp656";
+	$password = "m@tr966280";
+	$dbname = "chnlwysx_wp656";
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	
+	$sql = "SELECT STATUS FROM thermostation WHERE ID=0";
+	
+	$result = $conn->query($sql);
 
-	echo $_SERVER["boiler-status"];
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		echo $row["STATUS"];
+	}
+	} else {
+		echo "Error updating record: " . $conn->error;
+	}
+	
+	$conn->close();
 
 ?>
--- save-thermostation-data.php
+++ save-thermostation-data.php
@@ -1,9 +1,36 @@
 <?php
 header("Access-Control-Allow-Origin:*");
 header("Content-Type: text/plain");
+/*
 $filename="Status.json";
-$post = file_get_contents("php://input");
 $_SERVER["boiler-status"]=$post;
-echo $post;
 file_put_contents($filename, $post);
+*/
+$post = file_get_contents("php://input");
+
+
+$servername = "localhost";
+$username = "chnlwysx_wp656";
+$password = "m@tr966280";
+$dbname = "chnlwysx_wp656";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "UPDATE thermostation SET STATUS=$post WHERE ID=0";
+
+if ($conn->query($sql) === TRUE) {
+    //echo "Record updated successfully";
+    echo $post;
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+
+
 ?>
