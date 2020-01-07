d71bb18a25c1e89f3df15c0fefa8d10ac595fb97
cvirgili <cristiano.virgili@gmail.com>
2020-01-07 11:57:26 +0100

    ok

--- db-settings.php
+++ db-settings.php
@@ -1,7 +1,7 @@
 <?
-$servername = "localhost";
-$username = "chnlwysx_thermo";
-$password = "M@tr966280";
-$dbname = "chnlwysx_thermostation";
+$servername = "89.46.111.151";
+$username = "Sql1388337";
+$password = "pv1004e635";
+$dbname = "Sql1388337_1";
 $port=3306
 ?>
--- modules/Settings.js
+++ modules/Settings.js
@@ -1,12 +1,13 @@
 /*jshint esversion:6*/
+const host = "https://cristiano-virgili.me/thermostation"
 exports.esp01url = "http://192.168.1.10";
 exports.manualurl = "/hand/";
 exports.gpiourl = "/gpio/";
 exports.dataurl = "/data/";
 exports.status = "/status/";
-exports.savestatusurl = "https://virgili.netsons.org/save-thermostation-data.php";
-exports.saveschedulerurl = "https://virgili.netsons.org/save-scheduler.php";
-exports.getboilerdataurl = "https://virgili.netsons.org/read_boiler_data.php";
+exports.savestatusurl = `${host}/save-thermostation-data.php`;
+exports.saveschedulerurl = `${host}/save-scheduler.php`;
+exports.getboilerdataurl = `${host}/read_boiler_data.php`;
 //connectionString :`Data Source=62.149.153.28;Initial Catalog=MSSql187380;Integrated Security=False;User ID=MSSql187380;Connect Timeout=0;Encrypt=False;Packet Size=4096`
 //connectionString :`Driver={SQL Server Native Client 11.0};Server={62.149.153.28,1433};Database={MSSql187380};Uid={MSSql187380};Pwd={u4uc43436l};Trusted_Connection={#{trusted}};`
 exports.mssqlConfig = {
